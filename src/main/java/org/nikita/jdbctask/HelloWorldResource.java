80aea42e5e97b4eb67abed41f1471b805f26e694
NikitaDakuko <nikita.dakuko@gmail.com>
2024-06-30 14:39:33 +0300

    Test

--- src/main/java/org/nikita/jdbctask/HelloWorldResource.java
+++ src/main/java/org/nikita/jdbctask/HelloWorldResource.java
@@ -1,16 +1,20 @@
 package org.nikita.jdbctask;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("hello")
+@Path("/hello")
 public class HelloWorldResource {
 
 	@GET
 	public String sayHello() {
 		return "Hello World";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/{name}")
+	public String doGreeting(@PathParam("name") String someValue, @QueryParam("language") String language) {
+		return "Hello " + someValue + " with language " + language;
 	}
 }
---
+++ src/main/java/org/nikita/jdbctask/entity/Person.java
@@ -0,0 +1,6 @@
+package org.nikita.jdbctask.entity;
+
+public class Person {
+    private String name;
+    private int age;
+}
