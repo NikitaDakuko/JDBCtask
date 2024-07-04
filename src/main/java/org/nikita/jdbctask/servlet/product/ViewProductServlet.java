d13e99bfcb4a4a705b00cf30702cde95f0ed7ad3
NikitaDakuko <nikita.dakuko@gmail.com>
2024-07-04 16:36:58 +0300

    DB

--- src/main/java/org/nikita/jdbctask/servlet/product/ViewProductServlet.java
+++ src/main/java/org/nikita/jdbctask/servlet/product/ViewProductServlet.java
@@ -6,15 +6,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.nikita.jdbctask.dao.ProductDAO;
+import org.nikita.jdbctask.entity.Product;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/products")
 public class ViewProductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            ProductDAO db = new ProductDAO();
-            resp.getOutputStream().println(db.getAll().toString());
+        ProductDAO db = new ProductDAO();
+        List<Product> productList = db.getAll();
+        resp.getOutputStream().println(productList.toString());
     }
 }
 
