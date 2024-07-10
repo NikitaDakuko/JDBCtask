2363088f2eddf484f133cc3d367ffbcfd5960224
NikitaDakuko <nikita.dakuko@gmail.com>
2024-07-10 18:21:37 +0300

    Testing

--- src/main/java/org/nikita/jdbctask/servlet/orderapproval/ViewOrderApprovalServlet.java
+++ src/main/java/org/nikita/jdbctask/servlet/orderapproval/ViewOrderApprovalServlet.java
@@ -5,16 +5,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.nikita.jdbctask.dao.OrderApprovalDAO;
+import org.nikita.jdbctask.dao.OrderDetailDAO;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 @WebServlet("/orderApprovals")
 public class ViewOrderApprovalServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        OrderApprovalDAO db = new OrderApprovalDAO();
+        OrderDetailDAO db = new OrderDetailDAO();
 
         try {
             Long id = Long.parseLong(req.getParameter("id"));
